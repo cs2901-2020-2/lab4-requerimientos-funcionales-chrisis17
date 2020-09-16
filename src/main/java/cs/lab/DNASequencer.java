2afa19abd2fdb95e6f3764edb9a90f520f088c01
Christian Rojas <christian.rojas@utec.edu.pe>
2020-09-16 10:53:44 -0500

    working

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -14,9 +14,52 @@ public class  DNASequencer {
     public String calculate(List<String> part){
         String gnomes = getinput();
         StringBuffer result;
+        int initial = 0,next;
+        Boolean anexar = false, otherString = false, secondCorrection = false;
+        char firstLetter = ' ', nextLetter = ' ', temp;
 
-        for (char x : genomes){
-            result.append(x);
+        for (int x = 0; x < gnomes.length(); x++){
+            if (initial == 0){
+                result.append(gnomes.charAt(x));
+            }
+            if (anexar){
+                if (x == ' '){
+                    anexar = false;
+                }else{
+                    result.append(gnomes.charAt(x));
+                    secondCorrection = false;
+                }
+            }
+            if (secondCorrection){
+                if (x == nextLetter){
+                    anexar = true;
+                    result.append(temp);
+                }
+            }
+            if (otherString){
+                if (x == firstLetter){
+                    secondCorrection = true;
+                    temp = gnomes.charAt(x);
+                }
+                else{
+                    boolean findTheLetter = false;
+                    while (!findTheLetter){
+                        initial += 1;
+                        firstLetter = result.charAt(initial);
+                        nextLetter = result.charAt(initial + 1);
+                        if (x == firstLetter){
+                            secondCorrection = true;
+                            temp = gnomes.charAt(x);
+                            findTheLetter = true;
+                        }
+                    }
+                }
+            }
+            if (x == ' '){
+                firstLetter = result.charAt(initial);
+                nextLetter = result.charAt(initial + 1);
+                otherString = true;
+            }
         }
         
         return "result";
